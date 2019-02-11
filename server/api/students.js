55ce722ab086965345169e66860494ee9efaba44
vli8 <vli8@fordham.edu>
2019-02-11 15:22:17 -0500

    students all setup

--- server/api/students.js
+++ server/api/students.js
@@ -1,3 +1,13 @@
-const router = require("express").Router;
+const router = require("express").Router();
+const { Students, Campuses, db } = require("../db/index");
 
+router.get("/", async (req, res, next) => {
+  try {
+    const allStudents = await Students.findAll();
+    res.send(allStudents);
+  } catch (error) {
+    console.log("error");
+    res.status(404).send("ERROR");
+  }
+});
 module.exports = router;
--- server/index.js
+++ server/index.js
@@ -12,12 +12,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, "..", "public")));
 
+app.use("/api/students", require("./api/students")); // include our routes!
+app.use("/api/campuses", require("./api/campuses"));
+
 app.use((req, res, next) => {
   res.send("Error 404, no path found.").sendStatus(404);
 });
-
-app.use("/api/students", require("./api/students")); // include our routes!
-app.use("/api/campuses", require("./api/campuses"));
 
 app.listen(3000, async () => {
   console.log("Ready to listen");
